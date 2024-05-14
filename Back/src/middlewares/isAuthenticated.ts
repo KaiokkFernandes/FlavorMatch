7dc3799395d9808140550c184f33d8b64269e287
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-05-14 00:16:17 -0300

    Add authentication middleware to user details endpoint

---
+++ Back/src/middlewares/isAuthenticated.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+// Middleware de autenticação de usuario paginas como detalhes de usuario só podem ser acessadas por usuarios autenticados  por isso o middleware isAuthenticated  
+interface IPayload{
+    sub: string;    
+}
+
+export function isAuthenticated(
+    req: Request, 
+    res: Response, 
+    next: NextFunction  
+){
+    //Aqui eu recebo o token que e gerado no JWT quando usuario loga no sistema 
+    const authToken = req.headers.authorization;
+    if(!authToken){
+        return res.status(401).end();        
+    }
+    
+    const [, token] = authToken.split(" ");
+
+   try{
+      //Validação do token 
+      const {sub} = verify(
+        token,
+        process.env.JWT_SECRET!  
+
+      ) as IPayload;      
+      
+      return next();    
+
+   }catch(err){
+       return res.status(401).end();
+   }
+}
--- Back/src/routes.ts
+++ Back/src/routes.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from "express";
 import { CreateUserController } from "./controllers/createUserController";  
 import { AuthUsersController } from "./controllers/authUsersController"; 
 import { DetailUserController } from "./controllers/DetailUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
@@ -10,6 +11,7 @@ router.post('/users', new CreateUserController().handle)
 //Rota de autenticação de usuario  
 router.post('/session', new AuthUsersController().handle)  
 
-router.get('/me', new DetailUserController().handle )
+//Rota de detalhes do usuario   
+router.get('/me',  isAuthenticated, new DetailUserController().handle )
 
 export {router};
