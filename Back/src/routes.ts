f1b5ebfe5c6d5bb627c5e46de7088ab31aae1b74
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-05-13 23:41:14 -0300

    Adicionando Funcionalidade de geração de Token JWT na autenticação do usuario

--- Back/src/routes.ts
+++ Back/src/routes.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response } from "express";
 import { CreateUserController } from "./controllers/createUserController";  
+import { AuthUsersController } from "./controllers/authUsersController"; 
 
 const router = Router();
 
 router.post('/users', new CreateUserController().handle)
-
+router.post('/session', new AuthUsersController().handle)  
 
 
 export {router};
