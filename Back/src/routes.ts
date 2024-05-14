161a02382e7c4fa18bf55ee400a35a21d411b8aa
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-05-13 23:53:09 -0300

    Adiconando endpoint de detalhes do usuario

--- Back/src/routes.ts
+++ Back/src/routes.ts
@@ -1,11 +1,15 @@
 import { Router, Request, Response } from "express";
 import { CreateUserController } from "./controllers/createUserController";  
 import { AuthUsersController } from "./controllers/authUsersController"; 
+import { DetailUserController } from "./controllers/DetailUserController";
 
 const router = Router();
 
+//Rota de criação de usuario    
 router.post('/users', new CreateUserController().handle)
+//Rota de autenticação de usuario  
 router.post('/session', new AuthUsersController().handle)  
 
+router.get('/me', new DetailUserController().handle )
 
 export {router};
