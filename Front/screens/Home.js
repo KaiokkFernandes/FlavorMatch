be0bbc2e1213ff227cec28e2d24185851ceec37c
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-06-25 23:34:45 -0300

    Criação do Profile e instalação do axios

--- Front/screens/Home.js
+++ Front/screens/Home.js
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+// Importa React e os componentes necessários do React Native
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Header from '../componentes/Header/Header';
+// Importa o Header que você criou
+  // Ajuste o caminho conforme necessário
 
 export default function Home() {
   return (
-    <View>
-        
+    <View style={styles.container}>
+      <Header userImage="https://linkparaaimagem.com/foto.jpg" />
+      {/* Outros componentes da tela Home podem ser colocados aqui */}
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
